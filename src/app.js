7ad8cb8fcb56d5372705a44c8acefb5a2df0b140
zemr <rzedzicka@gmail.com>
2017-06-04 18:28:52 +0200

    Set up the basics

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        List
+      </div>
+    );
+  }
+}
+
+export default App;
