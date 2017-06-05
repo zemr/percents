5183b21ab2c28321af74ba9b0af2b033c191f60a
zemr <rzedzicka@gmail.com>
2017-06-05 16:19:20 +0200

    Display data

--- src/items/items-list.js
+++ src/items/items-list.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './items.css';
+
+const setStyle = (name) => {
+  const str = "width: 150px, height: 150px, position: absolute, opacity: .3, backgroundImage: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='30px' width='" + name.length * 14 + "px'><text x='4' y='20' fill='gray' font-family='monospace' font-size='20px'>" + name + "</text></svg>\")";
+  const pairs = str.split(', ');
+  const style = {};
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i].split(': ');
+    style[pair[0]] = pair[1];
+  }
+ return style;
+};
 
 const ItemsList = ({items}) => (
   <div>
     {
       items.map(item => (
-        <span key={item.id}>{item.tasks} </span>
+        item.id < 6 ?
+          <div className="avatar" key={item.id}>
+            <div style={setStyle(item.name)}></div>
+            <img src={item.avatar} alt={item.name} />
+          </div>
+          : null
       ))
     }
 
