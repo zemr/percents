45aa7a8ff00179fd28f9441300ff5a26a6d85dd7
zemr <rzedzicka@gmail.com>
2017-06-10 16:52:47 +0200

    Set sliders' values

--- src/items/items-view.js
+++ src/items/items-view.js
@@ -5,13 +5,31 @@ import ItemsList from './items-list';
 import Pagination from '../pagination/pagination';
 import Slider from '../slider/slider-view';
 
-const ItemsView = ({page}) => (
-  <div>
-    <Slider width={250} minValue={0} maxValue={200} number={1} />
-    <ItemsList items={data} start={page.start} end={page.end} />
-    <Pagination count={data.length} perPage={5} />
-  </div>
-);
+const tasks = [], repairs = [], efficiency = [];
+
+class ItemsView extends React.Component {
+  componentWillMount() {
+    for (let i = 0; i < data.length; i++) {
+      tasks.push(data[i].tasks);
+      repairs.push(data[i].repairs);
+      efficiency.push(data[i].efficiency);
+    }
+  }
+
+  render() {
+    const {page} = this.props;
+
+    return (
+      <div>
+        <Slider width={250} minValue={Math.min(...tasks)} maxValue={Math.max(...tasks)} number={1}/>
+        <Slider width={250} minValue={Math.min(...repairs)} maxValue={Math.max(...repairs)} number={2}/>
+        <Slider width={250} minValue={Math.min(...efficiency)} maxValue={Math.max(...efficiency)} number={3}/>
+        <ItemsList items={data} start={page.start} end={page.end}/>
+        <Pagination count={data.length} perPage={5}/>
+      </div>
+    );
+  }
+}
 
 export default connect(
   state => ({
