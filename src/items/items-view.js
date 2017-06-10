474cddcf41cdbee39f338970af668ea0919a0da3
zemr <rzedzicka@gmail.com>
2017-06-10 18:33:43 +0200

    Transfer pagination
    Now displays correct number of pages when filtering data.

--- src/items/items-view.js
+++ src/items/items-view.js
@@ -2,7 +2,6 @@ import React from 'react';
 import data from '../data/items.json';
 import { connect } from 'react-redux';
 import ItemsList from './items-list';
-import Pagination from '../pagination/pagination';
 import Slider from '../slider/slider-view';
 
 const tasks = [], repairs = [], efficiency = [];
@@ -25,7 +24,6 @@ class ItemsView extends React.Component {
         <Slider width={250} minValue={Math.min(...repairs)} maxValue={Math.max(...repairs)} number={2} />
         <Slider width={250} minValue={Math.min(...efficiency)} maxValue={Math.max(...efficiency)} number={3} />
         <ItemsList items={data} start={page.start} end={page.end} filters={sliders} />
-        <Pagination count={data.length} perPage={5} />
       </div>
     );
   }
