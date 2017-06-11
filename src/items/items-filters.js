be5266c59b629379c246756d30eb85b17d03f794
zemr <rzedzicka@gmail.com>
2017-06-11 16:36:55 +0200

    Refactor code

--- src/items/items-filters.js
+++ src/items/items-filters.js
@@ -3,11 +3,9 @@ import { connect } from 'react-redux';
 import ItemsList from './items-list';
 import Pagination from '../pagination/pagination';
 
-const ItemsFilters = ({data, start, end, filters}) => {
-  let items;
-
-  if (filters) {
-    const dataFiltered =
+export class ItemsFilters extends React.Component {
+  filterData(data, filters) {
+    return (
       data
         .filter(item =>
           item.tasks <= filters.max1
@@ -20,32 +18,51 @@ const ItemsFilters = ({data, start, end, filters}) => {
         .filter(item =>
           item.efficiency <= filters.max3
           && item.efficiency >= filters.min3
-        );
+        )
+    );
+  }
 
+  sliceData(dataFiltered, start, end) {
     if (dataFiltered.length <= start) {
-      items = dataFiltered.slice(0, end - start);
+      return dataFiltered.slice(0, end - start);
     } else {
-      items = dataFiltered.slice(start, end);
+      return dataFiltered.slice(start, end);
+    }
+  }
+
+  render() {
+    const { data, start, end, filters } = this.props;
+    let items, content;
+
+    if (filters) {
+      const dataFiltered = this.filterData(data, filters);
+      items = this.sliceData(dataFiltered, start, end);
+
+      content = (
+        <div>
+          <ItemsList items={items} />
+          <Pagination count={dataFiltered.length} perPage={5} />
+        </div>
+      )
+
+    } else {
+      items = this.sliceData(data, start, end);
+
+      content = (
+        <div>
+          <ItemsList items={items} />
+          <Pagination count={data.length} perPage={5} />
+        </div>
+      )
     }
 
     return (
       <div>
-        <ItemsList items={items} />
-        <Pagination count={dataFiltered.length} perPage={5} />
-      </div>
-    );
-
-  } else {
-    items = data.slice(start, end);
-
-    return (
-      <div>
-        <ItemsList items={items} />
-        <Pagination count={data.length} perPage={5} />
+        {content}
       </div>
     );
   }
-};
+}
 
 export default connect(
   state => ({
