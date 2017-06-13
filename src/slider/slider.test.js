71da9c547ee93c43f7c465e8efef8928a39b5cce
zemr <rzedzicka@gmail.com>
2017-06-14 00:11:11 +0200

    Add tests

--- src/slider/slider.test.js
+++ src/slider/slider.test.js
@@ -15,6 +15,12 @@ describe('slider', () => {
     expect(percentRoundDown).toBe('70%');
   });
 
+  it('calculates shifted percent values', () => {
+    const slider = TestUtils.renderIntoDocument(<Slider width={100} />);
+    const percent = slider.calculateValues(40, 10);
+    expect(percent).toBe('30%');
+  });
+
   it('calls parent function', () => {
     const handleMDStub = sinon.spy();
     const slider = TestUtils.renderIntoDocument(
@@ -23,14 +29,14 @@ describe('slider', () => {
         onMouseDown={handleMDStub}
       />
     );
-    const bar = TestUtils.scryRenderedDOMComponentsWithClass(slider, 'slider-bar');
-    TestUtils.Simulate.mouseDown(bar[1]);
+    const bar = TestUtils.findRenderedDOMComponentWithClass(slider, 'bottom');
+    TestUtils.Simulate.mouseDown(bar);
     expect(handleMDStub.calledOnce).toBeTruthy();
     handleMDStub.reset();
   });
 
   it('calculates relative position', () => {
-    const position = {touches: false, clientX: 50};
+    const position = { clientX: 50 };
     const handleMDStub = sinon.spy();
     const slider = TestUtils.renderIntoDocument(
       <Slider
@@ -39,10 +45,36 @@ describe('slider', () => {
         onMouseDown={handleMDStub}
       />
     );
-    const bar = TestUtils.scryRenderedDOMComponentsWithClass(slider, 'slider-bar');
-    TestUtils.Simulate.mouseDown(bar[1], position);
+    const bar = TestUtils.findRenderedDOMComponentWithClass(slider, 'bottom');
+    TestUtils.Simulate.mouseDown(bar, position);
     expect(handleMDStub.args[0]).toEqual([30]);
     handleMDStub.reset();
+  });
+
+  it('calculates relative position on touch devices', () => {
+    const position = { touches: [{clientX: 60}] };
+    const handleMDStub = sinon.spy();
+    const slider = TestUtils.renderIntoDocument(
+      <Slider
+        width={250}
+        left={20}
+        onMouseDown={handleMDStub}
+      />
+    );
+    const bar = TestUtils.findRenderedDOMComponentWithClass(slider, 'bottom');
+    TestUtils.Simulate.mouseDown(bar, position);
+    expect(handleMDStub.args[0]).toEqual([40]);
+    handleMDStub.reset();
+  });
+
+  it('returns relative position within a range', () => {
+    const MDStub = sinon.spy();
+    const slider = TestUtils.renderIntoDocument(<Slider width={200} left={10} onMouseDown={MDStub} />);
+    slider.handleMouseDown({ clientX: 5 });
+    expect(MDStub.args[0]).toEqual([0]);
+    slider.handleMouseDown({ clientX: 215 });
+    expect(MDStub.args[1]).toEqual([200]);
+    MDStub.reset();
   });
 
 });
