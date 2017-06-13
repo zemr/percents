d63735211e5b8a3a13d96dca08a2c5b01235a189
zemr <rzedzicka@gmail.com>
2017-06-13 20:15:01 +0200

    Adapt slider to its new background

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -12,8 +12,9 @@ class Slider extends React.Component {
     this.props.onMouseDown(posX);
   }
 
-  calculateValues(base) {
-    const value = Math.round(base / this.props.width * 10) * 10;
+  calculateValues(base, shift) {
+    let value = Math.round(base / this.props.width * 10) * 10;
+    if (shift) { value -= shift }
     return value + '%';
   }
 
@@ -31,19 +32,23 @@ class Slider extends React.Component {
           </span>
         </div>
 
-        <div className="slider-bars" id={"bar" + number}>
-          <div className="slider-bar bottom" onMouseDown={this.handleMouseDown}>
-            <div className="slider-bar active"
+        <div className="slider-bars">
+          <div className="slider-bar bottom transform" style={{width: width / 10 * 11}}>
+            <div className="slider-bar" id={"bar" + number} style={{width: width, left: width / 10 / 2 + 'px'}} onMouseDown={this.handleMouseDown}>
+              <div className="slider-bar active transform"
                  style={{
                    left: this.calculateValues(smallerValue),
-                   right: this.calculateValues(width - biggerValue)
+                   right: this.calculateValues(width - biggerValue, 10)
                  }}>
+              </div>
             </div>
           </div>
         </div>
 
-        <span className="slider-label max">{maxValue}</span>
-        <span className="slider-label min">{minValue}</span>
+        <div className="slider-labels-bottom">
+         <span className="slider-label max">{maxValue}</span>
+         <span className="slider-label min">{minValue}</span>
+        </div>
       </div>
     );
   }
